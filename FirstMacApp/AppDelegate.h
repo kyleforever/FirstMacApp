3f5040a195b6aca30ee2aaac7866ac24effc028c
LongLi <longli@LongLideMacBook-Pro.local>
2014-06-18 20:28:39 +0800

    Initial Commit

---
+++ FirstMacApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  FirstMacApp
+//
+//  Created by LongLi on 18/06/14.
+//  Copyright (c) 2014年 LongLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
